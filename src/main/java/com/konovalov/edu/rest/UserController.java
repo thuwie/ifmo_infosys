331bfde50836d2daed0617bc839aab2b5a5616aa
Ivan.Poliakov <Ivan.Poliakov@nexign-systems.com>
2019-04-05 00:18:57 +0300

    floawable: Introduce singleton
    Also changed:
    - UserController: Return 204 if user not found
    - UserController: Add CrossOrigin annotation
    - Update gitignore

--- src/main/java/com/konovalov/edu/rest/UserController.java
+++ src/main/java/com/konovalov/edu/rest/UserController.java
@@ -9,23 +9,59 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import java.util.List;
 
 @RestController
 public class UserController {
 
     private final UserDao userDao;
 
+    public static boolean isNullOrEmpty(String str) {
+        if(str != null && !str.isEmpty())
+            return false;
+        return true;
+    }
+
     @Autowired
     public UserController(UserDao userDao) {
         this.userDao = userDao;
     }
 
+    // TODO(ipolyakov): add demo task initiator here (POST)
+
+    @CrossOrigin
     @GetMapping(value = "/user/get/{userId}")
     @ResponseBody
     public ResponseEntity<User> getUserById(@PathVariable("userId") Integer id) {
 
         User user = userDao.getUserById(id);
+        if(user != null)
+            return new ResponseEntity<User>(user, HttpStatus.OK);
+        else
+            return new ResponseEntity<User>(user, HttpStatus.NO_CONTENT);
 
-        return new ResponseEntity<User>(user, HttpStatus.OK);
+    }
+
+    @CrossOrigin
+    @GetMapping(value = "/user/all")
+    @ResponseBody
+    public ResponseEntity<List<User>> getUsers() {
+
+        List<User> users = userDao.getAllUsers();
+
+        return new ResponseEntity<List<User>>(users, HttpStatus.OK);
+
+    }
+
+    @CrossOrigin
+    @PostMapping(value = "/authenticate")
+    @ResponseBody
+    public ResponseEntity<String> autheticate() {
+
+        String hello = "std::cout << \"Hallo, Leute!\"";
+
+        return new ResponseEntity<String>(hello, HttpStatus.OK);
     }
 }
