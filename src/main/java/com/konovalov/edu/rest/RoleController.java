7e53006cb62909c75ef1e003ae7cecaa57f295d0
Maksim Ya <kisamaksim@gmail.com>
2019-04-09 09:19:38 +0300

    Added CRUD for RoleController

--- src/main/java/com/konovalov/edu/rest/RoleController.java
+++ src/main/java/com/konovalov/edu/rest/RoleController.java
@@ -2,52 +2,62 @@ package com.konovalov.edu.rest;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.konovalov.edu.dao.RoleDao;
 import com.konovalov.edu.entity.Role;
+import lombok.AllArgsConstructor;
 
 @RestController
+@CrossOrigin
+@RequestMapping("/role")
+@AllArgsConstructor
 public class RoleController {
     
     private RoleDao roleDao;
     
-    @Autowired
-    public RoleController(RoleDao roleDao) {
-        this.roleDao = roleDao;
-    }
-    
-    @CrossOrigin
-    @GetMapping(value = "/role/{userId}")
+    @GetMapping(value = "/get/{roleId}")
     @ResponseBody
-    public ResponseEntity<Role> getRoleByUserId(@PathVariable("userId") Integer id) {
+    public ResponseEntity<Role> getRoleById(@PathVariable("roleId") Integer id) {
         Role roleById = roleDao.getRoleById(id);
         return new ResponseEntity<>(roleById, HttpStatus.OK);
     }
     
-    @CrossOrigin
-    @GetMapping(value = "/role/all")
+    @GetMapping(value = "/all")
     @ResponseBody
     public ResponseEntity<List<Role>> getAllRoles() {
         List<Role> allRoles = roleDao.getAllRoles();
         return new ResponseEntity<>(allRoles, HttpStatus.OK);
     }
     
-    @CrossOrigin
-    @PostMapping(value = "/role")
+    @PostMapping(value = "/addRole")
     @ResponseBody
     public ResponseEntity<Role> addRole(@RequestBody Role role) {
         roleDao.addRole(role);
         return new ResponseEntity<>(role, HttpStatus.OK);
+    }
+    
+    @PostMapping(value = "/updateRole")
+    public ResponseEntity<Role> updateRole(@RequestBody Role role) {
+        roleDao.updateRole(role);
+        Role updatedRole = roleDao.getRoleById(role.getRoleId());
+        return new ResponseEntity<>(updatedRole, HttpStatus.OK);
+    }
+    
+    @DeleteMapping("delete/{roleId}")
+    public HttpStatus deleteRole(@PathVariable("roleId") Integer id) {
+        roleDao.deleteRole(id);
+        return HttpStatus.OK;
     }
     
 }
